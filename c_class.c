8470f0235515448a31d83c8f134e6bf81e6536e5
MuhyulSuave <145707087+MuhyulSuave@users.noreply.github.com>
2023-10-24 13:22:18 +0100

    Update c_class.c

--- c_class.c
+++ c_class.c
@@ -1,7 +1,8 @@
-#include<stdio.h>
-
-int main()
-{
-printf("Hello\n");
-return 0;
+#include <stdio.h>
+int main(){
+    printf("I am learning C programming languages\n");
+    printf("I have just completed chapter 2\n");
+    printf("I am 99.9%% If percent ready");
+    printf("to the next chapter!\n");
+    return 0;
 }
